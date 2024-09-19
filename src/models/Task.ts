492c945af9a9cee9ff03f6a397447cd4c3456719
diegohuapayarivera <diego.huapaya@vallegrande.edu.pe>
2024-09-19 11:49:47 -0500

    add Model Task and relation to Model Project

---
+++ src/models/Task.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+export interface ITask extends Document {
+  name: string;
+  description: string;
+  project: Types.ObjectId;
+}
+
+export const TaskSchema: Schema = new Schema({
+  name: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true },
+  project: { type: Types.ObjectId, ref: "Project", required: true },
+});
+
+const Task = mongoose.model<ITask>("Task", TaskSchema);
+
+export default Task;
